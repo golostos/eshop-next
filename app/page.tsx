6fbff81c0082eaffea13b80eb57786a2a67ce5b1
Alexander Dmitriev <alex.golostos@gmail.com>
2024-04-03 22:28:24 +0300

    Without force dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import Catalog from "@/features/product/Catalog";
 import { Suspense } from "react";
 
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
+// Так как страница принимает searchParams, то она становится динамической
+// и без force-dynamic
 
 export default function Home({
   searchParams,
